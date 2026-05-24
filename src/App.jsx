d541e36c6c0df47bd60898c9ded6230bc4df7ac7
Shahid Zahoor Khan <shahi.khan.io1@gmail.com>
2026-05-24 12:17:46 +0500

    Fix phone number to international format +92 300 8307918

--- src/App.jsx
+++ src/App.jsx
@@ -960,7 +960,7 @@ function ContactSection() {
 
             {/* Phone */}
             <a
-              href="tel:+9203008307918"
+              href="tel:+923008307918"
               style={{
                 display: "flex",
                 alignItems: "center",
@@ -984,7 +984,7 @@ function ContactSection() {
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#D7E2EA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.45 2 2 0 0 1 3.59 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.56a16 16 0 0 0 5.53 5.53l.87-.87a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 21 16.92z"/>
               </svg>
-              0300 830 7918
+              +92 300 8307918
             </a>
           </div>
         </FadeIn>
